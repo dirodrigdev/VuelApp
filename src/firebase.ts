23d2df96d81c03904ab51eb01ed081555e91d044
dirodrigdev <diegorp.1978@gmail.com>
2025-12-07 20:18:13 +0100

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// src/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Config de tu proyecto (la que me pasaste)
+const firebaseConfig = {
+  apiKey: 'AIzaSyAZEy-9uo4BT0x4mMPlI9HgKvn8yhDNgcE',
+  authDomain: 'vuelapp-fd261.firebaseapp.com',
+  projectId: 'vuelapp-fd261',
+  storageBucket: 'vuelapp-fd261.firebasestorage.app',
+  messagingSenderId: '317272230186',
+  appId: '1:317272230186:web:a139ec3f97e074d378b653',
+  measurementId: 'G-XF1WFNNE0J',
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore (base de datos)
+export const db = getFirestore(app);
